6e97fef75625ecdebde911e677e553cc5a632c8e
Egor <mizerov-2003@mail.ru>
2021-05-07 13:06:43 +0500

    added method Valid for TokenManager

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,14 +1,17 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"strconv"
 	"time"
 )
 
 type TokenManager interface {
 	NewJWT(ttl time.Duration, id string, role string) (string, error)
 	Parse(accessToken string) (sub, role string, err error)
+	Valid(accessToken string) (bool, error)
 }
 
 const (
@@ -70,4 +73,41 @@ func (m *Manager) Parse(accessToken string) (sub, role string, err error) {
 	}
 
 	return
+}
+
+func (m *Manager) Valid(accessToken string) (bool, error) {
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(m.signingKey), nil
+	})
+	if err != nil {
+		return false, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return false, fmt.Errorf("error get user claims from token")
+	}
+
+	// get exp from token
+	exp, ok := claims["exp"].(string)
+	if !ok {
+		return false, fmt.Errorf("error get sub from token")
+	}
+
+	// parse exp(string) to int64
+	expInt, err := strconv.ParseInt(exp, 10, 64)
+	if err != nil {
+		return false, fmt.Errorf("error convertation exp to int")
+	}
+
+	// validate exp
+	if expInt > time.Now().Unix() {
+		return false, errors.New("token expired")
+	}
+
+	return true, nil
 }
